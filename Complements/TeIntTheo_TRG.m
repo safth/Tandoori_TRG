416b5f97109c8688c633d5205993c4e4c3b1cd7e
Simon Boivin <hackintosh@simons-imac.fil.umontreal.ca>
2018-02-06 11:04:50 -0500

    juste un ajout des fichiers de sorties .mat et du 2^0 dans le calcul du quenching.

--- Complements/TeIntTheo_TRG.m
+++ Complements/TeIntTheo_TRG.m
@@ -149,7 +149,7 @@ for t=1:length(Te)
 %% Mécanismes dépendant de la densité des 1s    
 [Depop2p,DepopRadFond,PopAutoAbs,DepopMix,PopMix,DepopSuperelestique,DepopIonisation,DepopNeutre,PopUpDown,PopNeutre] = TeDepopulation_Metastable_TRG(t+IndexOffset-1,gaz,ng,ne,Aij1s,Thetaij_1s,rate1s_2p,rateQuenching,rateNeutral,Br2p,nm_Ar);
 
-depopulation1s=Depop2p+DepopRadFond-PopAutoAbs+DepopMix-PopMix+DepopSuperelestique+DepopIonisation + DepopNeutre - PopUpDown;
+depopulation1s=Depop2p+DepopRadFond-PopAutoAbs+DepopMix-PopMix+DepopSuperelestique+DepopIonisation + DepopNeutre*2^(0) - PopUpDown;
 population1s=PopFond;
 % DepopNeutre;
 %% Obtention de la densité des niveaux pour avoir l'etat stationnaire depopulation=population
