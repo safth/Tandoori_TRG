07060056a18a31a48779328c1a2bc6d6ce7bd0ce
Simon Boivin <hackintosh@simons-imac.fil.umontreal.ca>
2018-02-09 11:41:40 -0500

    normal

--- Complements/TeDepopulation_Metastable_TRG.m
+++ Complements/TeDepopulation_Metastable_TRG.m
@@ -95,11 +95,11 @@ PopUpDown = ne*[0         0           0               0              0     ; %RI
 % elseif gaz == 5
 %     %Electronic Energy Transfer from Metastable Argon (4s3P2,0) to Xenon,
 %     %Oxygen and Chlorine Atoms. David L king 1973
-%    Gain_Ar1s3 = (nm_Ar(1,1,i,3)+ng(gaz)*1e-16)*ng(gaz)*1e-17*[ 10.53*0.0775 ;    %7d->6p
+%    Gain_Ar1s3 = nm_Ar(1,1,i,3)*ng(gaz)*1e-17*[ 10.53*0.0775 ;    %7d->6p
 %                                                 4.12*0.03*0.775; %5f->7d->6p
 %                                                 0.69*0.505];     %9s->6p
 %                     
-%     Gain_Ar1s5 = (nm_Ar(1,1,i,5)+ng(gaz)*1e-16)*ng(gaz)*1e-17*[16.78*0.68             ; %8d->6p
+%     Gain_Ar1s5 = nm_Ar(1,1,i,5)*ng(gaz)*1e-17*[16.78*0.68             ; %8d->6p
 %                                                16.78*0.001*0.02*0.78  ; %8d-9p-7d->6p
 %                                                16.78*0.001*0.2*0.51   ; %8d-9p-9s->6p
 %                                                8.82*0.022             ; %6f-8d->6p
